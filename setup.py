7a76146144bf5e0133eaad19edf17c57c24a8703
messense <messense@icloud.com>
2015-05-08 13:58:54 +0800

    Add setup scripts

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+import os
+from setuptools import setup, find_packages
+
+readme = 'README.md'
+if os.path.exists('README.rst'):
+    readme = 'README.rst'
+with open(readme) as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
+setup(
+    name='teambition',
+    version='0.1.0',
+    author='messense',
+    author_email='messense@icloud.com',
+    url='https://github.com/messense/teambition-api',
+    packages=find_packages(),
+    keywords='Teambition, SDK',
+    description='Teambition Python API',
+    long_description=long_description,
+    install_requires=requirements,
+    include_package_data=True,
+    tests_require=['nose', 'httmock'],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
