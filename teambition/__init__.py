59aee66083a705824f90c42d7ed2552d31fa2048
messense <messense@icloud.com>
2015-05-14 15:35:11 +0800

    Update exceptions

--- teambition/__init__.py
+++ teambition/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from teambition.client import Teambition  # NOQA
+from teambition.exceptions import TeambitionException  # NOQA
 
 
 __author__ = 'messense'
 __version__ = '0.1.0'
-__all__ = ['Teambition']
+__all__ = ['Teambition', 'TeambitionException']
--- teambition/exceptions.py
+++ teambition/exceptions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from requests import RequestException  # NOQA
+from requests import RequestException
 
 
-class TeambitionException(Exception):
+class TeambitionException(RequestException):
     pass
