a9068cc7f9fcd8e00d78fb0b4dd16fde7d9a8d15
messense <messense@icloud.com>
2015-05-15 18:30:31 +0800

    Update collections APIs

--- teambition/api/collections.py
+++ teambition/api/collections.py
@@ -11,28 +11,33 @@ class Collections(TeambitionAPI):
         """
         获取文件集
 
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-get
+
         :param id: 可选，文件集 ID
         :param parent_id: 可选，父级 ID
         :return: 返回的 JSON 数据包
         """
+        params = {}
         if id:
             endpoint = 'api/collections/{0}'.format(id)
-            params = {}
         elif parent_id:
             endpoint = 'api/collections'
-            params = {'_parentId': parent_id}
+            params['_parentId'] = parent_id
         return self._get(endpoint, params=params)
 
     def create(self, title, project_id, parent_id=None, description=None,
-               collection_type=None, color=None):
+               color=None):
         """
         新建文件集
+
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-create
 
         :param title: 文件集标题
         :param project_id: 项目 ID
         :param parent_id: 可选，父级 ID，默认为项目 ID
         :param description: 可选，描述
-        :param collection_type: 可选，类型，默认为空，保留字段：default, root
         :param color: 可选，颜色，默认为空
         :return: 返回的 JSON 数据包
         """
@@ -41,7 +46,6 @@ class Collections(TeambitionAPI):
             _projectId=project_id,
             _parentId=parent_id or project_id,
             description=description,
-            collectionType=collection_type,
             color=color
         )
         return self._post(
@@ -53,22 +57,27 @@ class Collections(TeambitionAPI):
         """
         删除文件集
 
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-delete
+
         :param id: 文件集 ID
         :return: 返回的 JSON 数据包
         """
         return self._delete('api/collections/{0}'.format(id))
 
     def update(self, id, title=None, parent_id=None, description=None,
-               color=None, is_archived=None):
+               color=None):
         """
         更新文件集
+
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-update
 
         :param id: 文件集 ID
         :param title: 可选，文件集标题
         :param parent_id: 可选，父级 ID
         :param description: 可选，描述
         :param color: 可选，颜色，默认为空
-        :param is_archived: 可选，是否归档
         :return: 返回的 JSON 数据包
         """
         data = optionaldict(
@@ -87,6 +96,9 @@ class Collections(TeambitionAPI):
         """
         归档文件集
 
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-archive
+
         :param id: 文件集 ID
         :return: 返回的 JSON 数据包
         """
@@ -96,7 +108,28 @@ class Collections(TeambitionAPI):
         """
         归档文件集
 
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-unarchive
+
         :param id: 文件集 ID
         :return: 返回的 JSON 数据包
         """
         return self._delete('api/collections/{0}/archive'.format(id))
+
+    def move(self, id, parent_id):
+        """
+        移动文件集
+
+        详情请参考
+        http://docs.teambition.com/wiki/collections#collections-move
+
+        :param id: 文件集 ID
+        :param parent_id: 新的父级目录 ID
+        :return: 返回的 JSON 数据包
+        """
+        return self._put(
+            'api/collections/{0}/move'.format(id),
+            data={
+                '_parentId': parent_id
+            }
+        )
