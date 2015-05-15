e1999b9935e3eaabb92f54cede0983f3124a3109
messense <messense@icloud.com>
2015-05-15 19:19:59 +0800

    Update tags APIs

--- teambition/api/tags.py
+++ teambition/api/tags.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from optionaldict import optionaldict
+
 from teambition.api.base import TeambitionAPI
 
 
@@ -9,6 +11,9 @@ class Tags(TeambitionAPI):
         """
         获取标签信息
 
+        详情请参考
+        http://docs.teambition.com/wiki/tags#tags-get
+
         :param id: 标签 ID
         :return: 返回的 JSON 数据包
         """
@@ -17,6 +22,9 @@ class Tags(TeambitionAPI):
     def create(self, name, project_id):
         """
         新建标签
+
+        详情请参考
+        http://docs.teambition.com/wiki/tags#tags-create
 
         :param name: 标签名称
         :param project_id: 项目 ID
@@ -29,6 +37,63 @@ class Tags(TeambitionAPI):
                 '_projectId': project_id
             }
         )
+
+    def delete(self, id):
+        """
+        删除标签
+
+        详情请参考
+        http://docs.teambition.com/wiki/tags#tags-delete
+
+        :param id: 标签 ID
+        :return: 返回的 JSON 数据包
+        """
+        return self._delete('api/tags/{0}'.format(id))
+
+    def update(self, id, name, color=None):
+        """
+        更新标签
+
+        详情请参考
+        http://docs.teambition.com/wiki/tags#tags-update
+
+        :param id: 标签 ID
+        :param name: 标签名称
+        :param color: 可选，颜色标记
+        :return: 返回的 JSON 数据包
+        """
+        data = optionaldict(
+            name=name,
+            color=color
+        )
+        return self._put(
+            'api/tags/{0}'.format(id),
+            data=data
+        )
+
+    def archive(self, id):
+        """
+        归档标签
+
+        详情请参考
+        http://docs.teambition.com/wiki/tags#tags-archive
+
+        :param id: 标签 ID
+        :return: 返回的 JSON 数据包
+        """
+        return self._post('api/tags/{0}/archive'.format(id))
+
+    def unarchive(self, id):
+        """
+        取消归档标签
+
+        详情请参考
+        http://docs.teambition.com/wiki/tags#tags-unarchive
+
+        :param id: 标签 ID
+        :return: 返回的 JSON 数据包
+        """
+        return self._delete('api/tags/{0}/archive'.format(id))
 
     def get_tasks(self, id):
         """
