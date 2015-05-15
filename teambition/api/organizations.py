936580737d29cc5996d79a506ad377f53c49112e
messense <messense@icloud.com>
2015-05-15 10:46:10 +0800

    Update organizations APIs

--- teambition/api/organizations.py
+++ teambition/api/organizations.py
@@ -11,6 +11,9 @@ class Organizations(TeambitionAPI):
         """
         获取组织信息
 
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-get
+
         :param id: 可选，组织 ID
         :return: 提供 id 返回特定组织信息，否则返回用户相关组织列表
         """
@@ -24,6 +27,9 @@ class Organizations(TeambitionAPI):
                location=None, website=None):
         """
         创建新组织
+
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-create
 
         :param name: 组织名字
         :param description: 可选，描述
@@ -48,7 +54,10 @@ class Organizations(TeambitionAPI):
         """
         删除组织
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-delete
+
+        :param id: 组织 ID
         :return: 返回的 JSON 数据包
         """
         return self._delete('api/organizations/{0}'.format(id))
@@ -58,7 +67,10 @@ class Organizations(TeambitionAPI):
         """
         更新组织信息
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-update
+
+        :param id: 组织 ID
         :param name: 组织名字
         :param description: 可选，描述
         :param logo: 可选，组织 logo
@@ -82,7 +94,10 @@ class Organizations(TeambitionAPI):
         """
         更新组织内项目顺序
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-update_projectids
+
+        :param id: 组织 ID
         :param project_ids: 项目 id 列表
         :return: 返回的 JSON 数据包
         """
@@ -97,7 +112,10 @@ class Organizations(TeambitionAPI):
         """
         添加组织成员
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-add-member
+
+        :param id: 组织 ID
         :param email: 邮箱或邮箱列表
         :return: 返回的 JSON 数据包
         """
@@ -114,7 +132,10 @@ class Organizations(TeambitionAPI):
         """
         删除组织成员
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-remove-member
+
+        :param id: 组织 ID
         :param user_id: 成员 ID
         :return: 返回的 JSON 数据包
         """
@@ -126,7 +147,10 @@ class Organizations(TeambitionAPI):
         """
         获取组织成员
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-get-member
+
+        :param id: 组织 ID
         :param user_id: 可选，成员 ID
         :return: 返回的 JSON 数据包
         """
@@ -140,7 +164,10 @@ class Organizations(TeambitionAPI):
         """
         获取组织推荐成员列表
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-recommend-members
+
+        :param id: 组织 ID
         :return: 返回的 JSON 数据包
         """
         return self._get('api/organizations/{0}/recommendMembers'.format(id))
@@ -149,7 +176,10 @@ class Organizations(TeambitionAPI):
         """
         更新组织成员角色
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-update-member
+
+        :param id: 组织 ID
         :param user_id: 组织成员 ID
         :param role_type: 角色类型, 可选值 member, admin, owner
         """
@@ -163,7 +193,10 @@ class Organizations(TeambitionAPI):
         """
         更新组织项目分组
 
-        :param id: 路径参数
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-dividers
+
+        :param id: 组织 ID
         :param dividers: 分组列表
         :return: 返回的 JSON 数据包
         """
@@ -178,7 +211,10 @@ class Organizations(TeambitionAPI):
         """
         获取组织统计数据
 
-        :param id: 路径参数
+        详情请参参考
+        http://docs.teambition.com/wiki/orgs#orgs-statistic
+
+        :param id: 组织 ID
         :param base: 可选，统计基于对象，可选值为 member, team, project，默认为 member
         :param start_date: 可选，开始日期，默认为一个月前
         :param end_date: 可选，结束日期，默认为今天
@@ -191,5 +227,105 @@ class Organizations(TeambitionAPI):
         )
         return self._get(
             'api/organizations/{0}/statistic'.format(id),
+            params=params
+        )
+
+    def get_projects(self, id, project_id=None, is_archived=False):
+        """
+        获取组织项目
+
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-projects
+
+        :param id: 组织 ID
+        :param project_id: 可选，项目 ID
+        :param is_archived: 可选，是否归档，默认为 False
+        :return: 返回的 JSON 数据包
+        """
+        if project_id:
+            endpoint = 'api/organizations/{0}/projects/{1}'.format(
+                id,
+                project_id
+            )
+        else:
+            endpoint = 'api/organizations/{0}/projects'
+        return self._get(
+            endpoint,
+            params={
+                'isArchived': is_archived
+            }
+        )
+
+    def quit(self, id):
+        """
+        退出组织
+
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-quit
+
+        :param id: 组织 ID
+        :return: 返回的 JSON 数据包
+        """
+        return self._put('api/organizations/{0}/quit'.format(id))
+
+    def resend_invitation(self, id, user_id):
+        """
+        重发邀请邮件
+
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-resend-invitation
+
+        :param id: 组织 ID
+        :param user_id: 成员 ID
+        :return: 返回的 JSON 数据包
+        """
+        return self._put(
+            'api/organizations/{0}/members{1}/resend'.format(id, user_id)
+        )
+
+    def get_member_tasks(self, id, member_id, start_date=None, is_done=None,
+                         all=False, page=1, count=30):
+        """
+        获取组织成员任务
+
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-get-member-tasks
+
+        :param id: 组织 ID
+        :param member_id: 成员 ID
+        :param start_date: 可选，起始日期，默认为当周的起始日期
+        :param is_done: 可选，是否完成，默认为 False
+        :param all: 可选，是否返回所有
+        :param page:  可选，页码
+        :param count: 可选，每页数量，默认为 30
+        :return: 返回的 JSON 数据包
+        """
+        params = optionaldict(
+            startDate=start_date,
+            isDone=is_done,
+            all=all,
+            page=page,
+            count=count
+        )
+        return self._get(
+            'api/organizations/{0}/members/{1}/tasks'.format(id, member_id),
+            params=params
+        )
+
+    def get_member_events(self, id, member_id, start_date=None):
+        """
+        获取组织成员日程
+
+        详情请参考
+        http://docs.teambition.com/wiki/orgs#orgs-get-member-events
+
+        :param id: 组织 ID
+        :param member_id: 成员 ID
+        :param start_date: 可选，起始日期，默认为当周的起始日期
+        :return: 返回的 JSON 数据包
+        """
+        params = optionaldict(startDate=start_date)
+        return self._get(
+            'api/organizations/{0}/members/{1}/events'.format(id, member_id),
             params=params
         )
