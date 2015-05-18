d0e9eeff101f802668c2eaf7c937df076bad7f26
messense <messense@icloud.com>
2015-05-18 14:12:22 +0800

    Bump version to 0.1.1

--- teambition/__init__.py
+++ teambition/__init__.py
@@ -4,6 +4,7 @@ from teambition.client import Teambition  # NOQA
 from teambition.exceptions import TeambitionException  # NOQA
 
 
-__author__ = 'messense'
-__version__ = '0.1.0'
 __all__ = ['Teambition', 'TeambitionException']
+
+__author__ = 'messense'
+__version__ = '0.1.1'
