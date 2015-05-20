2e14a82314094fb87f82ca546c4945bad69be2a5
messense <messense@icloud.com>
2015-05-20 15:09:55 +0800

    Add __str__ and __repr__ in TeambitionException

--- teambition/exceptions.py
+++ teambition/exceptions.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import six
 from requests import RequestException
 
 
 class TeambitionException(RequestException):
-    pass
+
+    def __str__(self):
+        if six.PY2:
+            return self.response.content
+        else:
+            return self.response.text
+
+    __repr__ = __str__
