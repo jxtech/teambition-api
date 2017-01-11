fb32b8ba622d72c513c326e4268ac6c1d4a7e673
yuwensheng <yuwensheng@renrenche.com>
2017-01-11 14:24:54 +0800

    update format string

--- teambition/api/tasks.py
+++ teambition/api/tasks.py
@@ -427,7 +427,7 @@ class Tasks(TeambitionAPI):
         :param id: 任务 ID
         :return: 返回的 JSON 数据包
         """
-        return self._get('api/tasks/{id}/activities'.format(**locals()))
+        return self._get('api/tasks/{0}/activities'.format(id))
 
     def import_tasks(self, tasklist_id, tasks, stage_id=None, executor_id=None,
                      involve_members=None, due_date=None, visiable=None):
